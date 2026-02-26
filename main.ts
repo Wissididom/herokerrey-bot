4d60713c0b93ca735b1c1fe9e9a1f7e746566de5
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-02-26 23:16:34 +0000

    Add logic to resolve user for birthday bot (#50)

--- main.ts
+++ main.ts
@@ -66,6 +66,32 @@ client.on(Events.ClientReady, async () => {
 });
 
 client.on(Events.MessageCreate, async (msg) => {
+  if (msg.author.id == "656621136808902656" && msg.embeds.length > 0) {
+    const embedDescription = msg.embeds[0].description;
+    if (!embedDescription) return;
+    const userRegex = /<@!?(\d+)>/g;
+    const userIds = [...embedDescription.matchAll(userRegex)].map((m) => m[1]);
+    const users = await Promise.all(userIds.map(async (id) => {
+      try {
+        return await client.users.fetch(id);
+      } catch {
+        return null;
+      }
+    }));
+    const usernames = users.filter((u): u is NonNullable<typeof u> =>
+      u !== null
+    ).map((u) => u.username);
+    if (usernames.length < 1) return;
+    try {
+      await msg.channel.send({ content: usernames.join(", ") });
+    } catch (err) {
+      console.error(
+        "Could not send message for resolving birthday bot mentions",
+        err,
+      );
+    }
+    return;
+  }
   if (msg.author.bot) return; // skip messages by bots
   if (await handlePepe(msg, false)) {
     await handleYuh(msg, false);
